40160d010674616f4ce3de844244fa344b996292
Konstantin Krastev <grizmin@gmail.com>
2018-05-17 03:23:46 +0300

    added config replacement option

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ for config_path in nginx_configs.items():
 for j2file in glob.glob('/etc/nginx/templates/*.j2'):
     cfname = "/etc/nginx/conf.d/{}".format(os.path.splitext(os.path.basename(j2file))[0])
     if not os.path.exists(cfname) or args['REWRITE_DEFAULT_CONFIG']:
-        convert(j2file, cfname)
+        convert(j2file, cfname, args)
 
 #copy proxy conf files
 for file in glob.glob('/etc/nginx/templates/*.conf'):
